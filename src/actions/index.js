1fba48c4953a3e218cc36fc98ce82def6a379336
hanus-kostiantyn <hanus.kostiantyn@gmail.com>
2024-09-05 12:40:07 +0200

    Apply 'configureStore', 'createAction', 'createReducer' to the project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,51 @@
-export const heroesFetching = () => {
-    return {
-        type: "HEROES_FETCHING",
-    };
+import { createAction } from "@reduxjs/toolkit";
+
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then((data) => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()));
 };
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: "HEROES_FETCHED",
-        payload: heroes,
-    };
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+        .then((data) => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()));
 };
 
-export const heroesFetchingError = () => {
-    return {
-        type: "HEROES_FETCHING_ERROR",
-    };
-};
+// export const heroesFetching = () => {
+//     return {
+//         type: "HEROES_FETCHING",
+//     };
+// };
+
+export const heroesFetching = createAction("HEROES_FETCHING");
+
+// export const heroesFetched = (heroes) => {
+//     return {
+//         type: "HEROES_FETCHED",
+//         payload: heroes,
+//     };
+// };
+
+export const heroesFetched = createAction("HEROES_FETCHED");
+
+// export const heroesFetchingError = () => {
+//     return {
+//         type: "HEROES_FETCHING_ERROR",
+//     };
+// };
+
+export const heroesFetchingError = createAction("HEROES_FETCHING_ERROR");
 
 export const filtersFetching = () => {
     return {
         type: "FILTERS_FETCHING",
     };
 };
+
+// export const filtersFetching = createAction("FILTERS_FETCHING");
 
 export const filtersFetched = (filters) => {
     return {
@@ -30,11 +54,15 @@ export const filtersFetched = (filters) => {
     };
 };
 
+// export const filtersFetched = createAction("FILTERS_FETCHED");
+
 export const filtersFetchingError = () => {
     return {
         type: "FILTERS_FETCHING_ERROR",
     };
 };
+
+// export const filtersFetchingError = createAction("FILTERS_FETCHING_ERROR");
 
 export const activeFilterChanged = (filter) => {
     return {
@@ -43,16 +71,22 @@ export const activeFilterChanged = (filter) => {
     };
 };
 
-export const heroCreated = (hero) => {
-    return {
-        type: "HERO_CREATED",
-        payload: hero,
-    };
-};
+// export const activeFilterChanged = createAction("ACTIVE_FILTER_CHANGED");
 
-export const heroDeleted = (id) => {
-    return {
-        type: "HERO_DELETED",
-        payload: id,
-    };
-};
+// export const heroCreated = (hero) => {
+//     return {
+//         type: "HERO_CREATED",
+//         payload: hero,
+//     };
+// };
+
+export const heroCreated = createAction("HERO_CREATED");
+
+// export const heroDeleted = (id) => {
+//     return {
+//         type: "HERO_DELETED",
+//         payload: id,
+//     };
+// };
+
+export const heroDeleted = createAction("HERO_DELETED");
